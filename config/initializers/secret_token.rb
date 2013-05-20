2f01e9745213ef38c1f315119b725f0e3214d7e7
Spencer Montgomery <smontgo5@gmail.com>
2013-05-20 15:53:30 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dingle::Application.config.secret_key_base = '9a7747132f6f7f1e4505899724fd1fd1e7f3a3d69f63d86732e98ea4d14db5a4c9520fafcefd1c09f21ba36b502966d9ae2013b1f68e7c15261697f57c369ea2'
